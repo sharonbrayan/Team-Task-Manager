1931f2607097d8bf0b1d50804493e85e39831b31
sharonbrayan <sharonbrayan087@gmail.com>
2026-06-06 17:05:42 +0530

    Revert "fix: variable initialization"
    This reverts commit 2e424e109c46b122a83a9f2c639d5ffe688de8cc.

--- server/controllers/authcontroller.mjs
+++ server/controllers/authcontroller.mjs
@@ -100,7 +100,7 @@ export const logout = (req, res) => {
 // --------------------
 export const getMe = async (req, res, next) => {
     try {
-        res.clearCookie('token', token,{
+        res.clearCookie('token', {
             httpOnly: true,
             secure: isProduction,
             sameSite: isProduction ? "none" : "lax",
